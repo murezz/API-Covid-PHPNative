f2e990200911572923d1eca6da564d4b6815091b
rejakartans <rezaaditya.131202@gmail.com>
2021-06-15 23:31:45 +0700

    make it better

--- src/layouts/navbar.php
+++ src/layouts/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #56e0f5;" data-aos="fade-down">
     <div class="container">
-        <a class="navbar-brand" href="https://github.com/rejakartans">Rejakartans</a>
+        <a class="navbar-brand" target="_blank" href="https://github.com/murezz">Murezz.</a>
         <div class="d-flex justify-content-end">
             <a class="navbar-brand" href="">Data Covid 19</a>
         </div>
